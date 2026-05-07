1325a7970c14345a7fd6b67e7adfa147c897440f
hxttm <S10273775@connect.np.edu.sg>
2026-05-07 15:31:05 +0800

    task1

--- Assets/scripts/CollisionScript.cs
+++ Assets/scripts/CollisionScript.cs
@@ -6,5 +6,21 @@ public class CollisionScript : MonoBehaviour
     {
         print("Collision detected with" 
         + collision.gameObject.name);
+        if(collision.gameObject.name=="NestedParent_Unpack")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        print("Collision ended with"
+        + collision.gameObject.name);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        print("Colliding with"
+        + collision.gameObject.name);
     }
 }
