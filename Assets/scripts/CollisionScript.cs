0d6609c045d72da434617301f3822e2ac6405a5c
hxttm <S10273775@connect.np.edu.sg>
2026-05-07 13:50:47 +0800

    collision script

---
+++ Assets/scripts/CollisionScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    Void OncollisionEnter (Collisiom collision)
+    {
+        print("Collided detected!");
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
