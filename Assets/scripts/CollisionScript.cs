b796175daac8f7828cba829b090051ccce52f85c
hxttm <S10273775@connect.np.edu.sg>
2026-05-28 21:47:05 +0800

    task1 help

--- Assets/scripts/CollisionScript.cs
+++ Assets/scripts/CollisionScript.cs
@@ -14,17 +14,9 @@ public class CollectibleScript : MonoBehaviour
         collectibleAudio = GetComponent<AudioSource>(); // Get the AudioSource component attached to this GameObject so we can play a sound when collected
     }
 
-    public void Collect() // Custom method to handle the collection of this item, called from the PlayerScript when the player interacts with it
-    {
-        // if(collectibleAudio != null) // Check if the AudioSource component exists before trying to play a sound
-        // {
-        //     collectibleAudio.Play(); // Play the collection sound effect to provide feedback to the player
-        // }
-        // SOLUTION 1
-        //Destroy(gameObject, collectibleAudio.clip.length); // Destroy this GameObject after the sound has finished playing to remove it from the scene
-    
-        // SOLUTION 2
-        AudioSource.PlayClipAtPoint(collectibleAudioClip, transform.position);
-        Destroy(gameObject); // Immediately destroy this GameObject to remove it from the scene, allowing the sound to play independently
+    public void Collect() 
+    { 
+        AudioSource.PlayClipAtPoint(collectibleAudioClip, transform.position); Destroy(gameObject);
     }
-}
+    
+    }
